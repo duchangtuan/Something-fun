532ae3efa13fed696c525d7619f8882459b76c2b
10.204.5.47 <fengxi1986@gmail.com>
2016-07-24 18:00:53 +0800

    The source code of reduce in python from offical websit

---
+++ python/reduce.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+def reduce(function, iterable, initializer=None):
+    it = iter(iterable)
+    if initializer is None:
+        try:
+            initializer = next(it)
+        except StopIteration:
+            raise TypeError('reduce() of empty sequence with no initial value')
+    accum_value = initializer
+    for x in it:
+        accum_value = function(accum_value, x)
+    return accum_value
